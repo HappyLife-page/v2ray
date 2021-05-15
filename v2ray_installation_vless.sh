5e7958391d1105751896edbddbaea71ba25007c5
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2021-05-15 10:24:12 +0800

    Update v2ray_installation_vless.sh

--- v2ray_installation_vless.sh
+++ v2ray_installation_vless.sh
@@ -4,88 +4,82 @@
 # Plat: ubuntu 18.04+
 # Eg  : bash v2ray_installation_vless.sh "你的域名"
 
-##安装依赖包
-apt update
-apt install curl pwgen openssl netcat cron socat -y
-
-domainName="$1"
-port="`shuf -i 20000-65000 -n 1`"
-uuid="`uuidgen`"
-path="/`pwgen -A0 8 5 | xargs |sed 's/ /\//g'`"
-
-if [ -z "$domainName" ];then
+if [ -z "$1" ];then
 	echo "域名不能为空"
 	exit
 fi
 
-
-##配置系统时区为东八区(其实如果只做代理用的话没什么必要,反而会导致你本地时间和代理时间不一致)
+# 配置系统时区为东八区
 rm -f /etc/localtime
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 
-##使用v2ray官方命令安装v2ray
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
-
-bash install-release.sh
-bash install-dat-release.sh
-
-systemctl enable v2ray
-
-
-##安装nginx
-apt install nginx -y
+# 使用ubuntu官方源安装nginx和依赖包并设置开机启动
+apt update
+apt install nginx curl pwgen openssl netcat cron socat -y
 systemctl enable nginx
 systemctl start nginx
 
 
+# 开始部署之前，我们先配置一下需要用到的参数，如下：
+# "域名，端口，uuid，ws路径，ssl证书目录"
+# 1.设置你的解析好的域名
+domainName="$1"
+# 2.随机生成v2ray需要用到的服务端口
+port="`shuf -i 20000-65000 -n 1`"
+# 3.随机生成一个uuid
+uuid="`uuidgen`"
+# 4.随机生成并创建一个websocket需要使用的目录path
+path="$(mkdir -pv "/`pwgen -A0 6 8 | xargs |sed 's/ /\//g'`" |awk -F"'" END'{print $2}')"
+# 5.以时间为基准随机创建一个存放ssl证书的目录
+ssl_dir="$(mkdir -pv "/usr/local/etc/v2ray/ssl/`date +"%F-%H-%M-%S"`" |awk -F"'" END'{print $2}')"
+
+
+# 使用v2ray官方命令安装v2ray并设置开机启动
+curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+bash install-release.sh
+bash install-dat-release.sh
+systemctl enable v2ray
+
+
 ##安装acme,并申请加密证书
-## ssl_dir="`mkdir -pv /usr/local/etc/v2ray/ssl | awk -F"'" 'END{print $2}'`"
-ssl_dir="/usr/local/etc/v2ray/ssl";! [ -d $ssl_dir ] && mkdir -p $ssl_dir
 source ~/.bashrc
 curl  https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --issue -d "$domainName" --alpn -k ec-256
 ~/.acme.sh/acme.sh --installcert -d "$domainName" --fullchainpath $ssl_dir/v2ray.crt --keypath $ssl_dir/v2ray.key --ecc
-chmod www-data.www-data $ssl_dir/v2ray.*
+chown www-data.www-data $ssl_dir/v2ray.*
 
 
-##创建WS路径,配置v2ray时会用到[目录可以自定义]
-mkdir -pv "$path" && chmod -R 644 "$path"
-
-
-##添加v2ray的nginx配置文件
+# 配置nginx【如下80服务块完全可以不需要】，执行如下命令即可添加nginx配置文件
 echo "
 server {
 	listen 80;
-        server_name "$domainName";
-        return 301 https://"'$host$request_uri'";
+	server_name "$domainName";
+	return 301 https://"'$host'""'$request_uri'";
 }
-
 server {
-        listen 443 ssl http2 default_server;
-        listen [::]:443 ssl http2 default_server;
-        server_name "$domainName";
-        ssl_certificate $ssl_dir/v2ray.crt;
-        ssl_certificate_key $ssl_dir/v2ray.key;
-        ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
-        ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
-
-        root /usr/share/nginx/html;
-    
-        location "$path" {
-	        proxy_redirect off;
-	        proxy_pass http://127.0.0.1:"$port";
-	        proxy_http_version 1.1;
-	        proxy_set_header Upgrade "'"$http_upgrade"'";
-	        proxy_set_header Connection '"'upgrade'"';
-	        proxy_set_header Host "'"$http_host"'";
-        }
-
+	listen 443 ssl http2 default_server;
+	listen [::]:443 ssl http2 default_server;
+	server_name "$domainName";
+	ssl_certificate $ssl_dir/v2ray.crt;
+	ssl_certificate_key $ssl_dir/v2ray.key;
+	ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
+	ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+	root /usr/share/nginx/html;
+	
+	location "$path" {
+		proxy_redirect off;
+		proxy_pass http://127.0.0.1:"$port";
+		proxy_http_version 1.1;
+		proxy_set_header Upgrade "'"$http_upgrade"'";
+		proxy_set_header Connection '"'upgrade'"';
+		proxy_set_header Host "'"$http_host"'";
+	}
 }
 " > /etc/nginx/conf.d/v2ray.conf
 
-##配置v2ray
+# 配置v2ray，执行如下命令即可添加v2ray配置文件
 echo '
 {
   "log" : {
@@ -102,13 +96,11 @@ echo '
       "clients": [
         {
           "id": '"\"$uuid\""',
-          "level": 1,
-	  "email": ""
+          "level": 1
         }
       ]
     },
    "streamSettings":{
-      "decryption":"none",
       "network": "ws",
       "wsSettings": {
            "path": '"\"$path\""'
@@ -133,6 +125,7 @@ echo '
   "routing": {
     "strategy": "rules",
     "settings": {
+      "decryption":"none",
       "rules": [
         {
           "type": "field",
@@ -145,7 +138,7 @@ echo '
 }
 ' > /usr/local/etc/v2ray/config.json
 
-##重启v2ray和nginx
+# 重启v2ray和nginx
 systemctl restart v2ray
 systemctl status -l v2ray
 /usr/sbin/nginx -t && systemctl restart nginx
