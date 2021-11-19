f0daa291adb1f88e66a8eb94257efd7980939073
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2021-11-19 08:52:58 +0800

    Update v2ray_installation_vless.sh

--- v2ray_installation_vless.sh
+++ v2ray_installation_vless.sh
@@ -29,8 +29,8 @@ domainName="$1"
 port="`shuf -i 20000-65000 -n 1`"
 # 3.随机生成一个uuid
 uuid="`uuidgen`"
-# 4.随机生成并创建一个websocket需要使用的目录path
-path="$(mkdir -pv "/`pwgen -A0 6 8 | xargs |sed 's/ /\//g'`" |awk -F"'" END'{print $2}')"
+# 4.随机生成一个websocket需要使用的path
+path="/`pwgen -A0 6 8 | xargs |sed 's/ /\//g'`"
 # 5.以时间为基准随机创建一个存放ssl证书的目录
 ssl_dir="$(mkdir -pv "/usr/local/etc/v2ray/ssl/`date +"%F-%H-%M-%S"`" |awk -F"'" END'{print $2}')"
 
