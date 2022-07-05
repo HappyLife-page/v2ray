ba94066a207f104ee05256afa6a800628478ddfa
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2022-07-05 21:24:44 +0800

    Update v2ray_installation_vmess.sh
    添加uuidgen命令所需的包uuid-runtime

--- v2ray_installation_vmess.sh
+++ v2ray_installation_vmess.sh
@@ -17,7 +17,7 @@ cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 # 使用ubuntu官方源安装nginx和依赖包并设置开机启动，关闭防火墙ufw
 apt update
-apt install nginx curl pwgen openssl netcat cron -y
+apt install nginx curl pwgen openssl netcat cron uuid-runtime -y
 systemctl enable nginx
 systemctl start nginx
 ufw disable
