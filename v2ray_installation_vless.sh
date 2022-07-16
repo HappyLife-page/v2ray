2ce2aeefe701a8686fe2a96911b0d43062b4917e
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2022-07-16 09:37:41 +0800

    Update v2ray_installation_vless.sh
    修正dpkg中断问题

--- v2ray_installation_vless.sh
+++ v2ray_installation_vless.sh
@@ -15,6 +15,7 @@ cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 
 # 使用ubuntu官方源安装nginx和依赖包并设置开机启动，关闭防火墙ufw
+/usr/bin/dpkg --configure -a
 apt clean all && apt update
 apt install nginx curl pwgen openssl netcat cron uuid-runtime -y
 systemctl enable nginx
