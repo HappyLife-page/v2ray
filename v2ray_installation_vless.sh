af575429b63cd8bea10a91d2b630d155d2df8079
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2023-01-16 12:44:08 +0800

    Update v2ray_installation_vless.sh
    v2ray官方从5.1.0变更到5.20，配置大变化，太不靠谱了。
    本安装脚本修正指定为5.1.0版，后续会持续跟进稳定版，不再追新。。

--- v2ray_installation_vless.sh
+++ v2ray_installation_vless.sh
@@ -41,10 +41,7 @@ ssl_dir="$(mkdir -pv "/usr/local/etc/v2ray/ssl/`date +"%F-%H-%M-%S"`" |awk -F"'"
 
 
 # 使用v2ray官方命令安装v2ray并设置开机启动
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
-bash install-release.sh
-bash install-dat-release.sh
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --version 5.1.0
 systemctl enable v2ray
 
 # 修正官方5.1+版本安装脚本启动命令错误
